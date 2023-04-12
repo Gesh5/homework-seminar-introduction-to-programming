e6f4cc9051be0581186307b1af2d8c3621bf6884
Andrey <gesh_92@mail.ru>
2023-04-12 19:54:51 +1000

    добавил код для решения задачи 1

--- task 001/Program.cs
+++ task 001/Program.cs
@@ -4,3 +4,23 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+Console.Write("введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int max = A;
+int min = A;
+
+if (A > max) max = A;
+if (B > max) max = B;
+
+Console.Write("max= ");
+Console.WriteLine(max);
+Console.Write("min= ");
+Console.WriteLine(min);
+
+Console.WriteLine("задача выполнена");
+Console.ReadLine();
+
+Console.WriteLine("поздравляю, вы успешно выполнили задание 1");
